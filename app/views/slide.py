efc76cadb7767b5ab6da9852396b142302f28db3
Markus Obendrauf <markus.obendrauf@tngtech.com>
2023-09-15 16:06:13 +0200

    implement slideshow

--- app/views/slide.py
+++ app/views/slide.py
@@ -9,31 +9,44 @@ class TransitionClass(Enum):
     PREVIOUS = "previous-slide"
 
 
-slides = ["slide.html"]
+def slide_base(request: WSGIRequest, slide_number: int = 0) -> HttpResponse:
+    print("here")
+    previous = request.GET.get("previous") or False
+    transition_class = TransitionClass.PREVIOUS if previous else TransitionClass.NEXT
+    previous_slide_number = slide_number - 1
+    next_slide_number = slide_number + 1
+    return render(
+        request,
+        "slide_base.html",
+        {
+            "transition_class": transition_class.value,
+            "previous_slide_number": previous_slide_number,
+            "next_slide_number": next_slide_number,
+        },
+    )
 
 
-def next_slide(request: WSGIRequest, slide_number: int = 0) -> HttpResponse:
-    return render_slide(request, slide_number + 1, TransitionClass.NEXT)
-
-
-def previous_slide(request: WSGIRequest, slide_number: int = 0) -> HttpResponse:
-    return render_slide(request, slide_number - 1, TransitionClass.PREVIOUS)
+slides = [
+    "slide_1.html",
+    "incrementing_button/demo.html",
+    "lazy_load/lazy_load_demo.html",
+    "infinite_scroll/demo.html",
+    "edit/demo.html",
+]
 
 
 def slide(request: WSGIRequest, slide_number: int = 0) -> HttpResponse:
-    return render_slide(request, slide_number)
-
-
-def render_slide(
-    request: WSGIRequest,
-    slide_number: int,
-    transition_class: TransitionClass = TransitionClass.NEXT,
-) -> HttpResponse:
+    previous = request.GET.get("previous") or False
+    transition_class = TransitionClass.PREVIOUS if previous else TransitionClass.NEXT
+    slide = slides[slide_number]
+    previous_slide_number = slide_number - 1
+    next_slide_number = slide_number + 1
     return render(
         request,
-        "slide.html",
+        slide,
         {
             "transition_class": transition_class.value,
-            "page": "incrementing_button/button.html",
+            "previous_slide_number": previous_slide_number,
+            "next_slide_number": next_slide_number,
         },
     )
