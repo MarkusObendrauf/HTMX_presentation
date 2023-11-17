11c797a098150e3c926dfef1199821561a8ce7b3
Markus Obendrauf <markus.obendrauf@tngtech.com>
2023-11-17 09:26:59 +0100

    remove old contact model

--- app/models.py
+++ app/models.py
@@ -1,14 +1,6 @@
 from django.db import models
 
 
-class Contact(models.Model):
-    name = models.CharField(max_length=200)
-    phone_number = models.CharField(max_length=200)
-
-    def __str__(self):
-        return self.name
-
-
 class Contact2(models.Model):
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
